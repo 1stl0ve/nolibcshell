7331fa8758f4208d384fac09bafb2b3c4dbafd5e
Leo St. Amour <leo.c.stamour@gmail.com>
2020-04-15 17:32:50 -0400

    Added documentation to strings.c; removed memcpy()

--- strings.c
+++ strings.c
@@ -1,17 +1,25 @@
-#include <stdlib.h>
-
+/**
+ * A collection of useful string functions.
+ * 
+ * NOTE: all of these functions assume a properly formed C string with a terminating
+ * NULL byte.
+ * 
+ */
+#include "mylib.h"
 #include "strings.h"
 #include "syscall.h"
 
-void zero_string(char *str, unsigned length)
-{
-    int i = 0;
-    for (i = 0; i < length; i++)
-    {
-        str[i] = 0;
-    }
-}
-
+/**
+ * strncmp:
+ * @brief checks each value in a string for equality.
+ * 
+ * This function compares up to n bytes of two strings and checks if each value is equal.
+ * 
+ * @param s1 - string one.
+ * @param s2 - string two.
+ * @param n  - the number of bytes to compare.
+ * @return 0 if s1 == s2; -1 otherwise.
+ */
 int strncmp(const char *s1, const char *s2, size_t n)
 {
     int i = 0;
@@ -25,16 +33,29 @@ int strncmp(const char *s1, const char *s2, size_t n)
     return 0;
 }
 
+/**
+ * strlen:
+ * @brief returns the number of bytes in a string (excluding the terminating NULL byte).
+ * 
+ * @param s - the string
+ * @return the number of bytes in s.
+ */
 size_t strlen(const char *s)
 {
     size_t i = 0;
     char *pos = (char *)s;
 
+    /**
+     * a NULL string has no length.
+     */
     if (!pos)
     {
         return 0;
     }
 
+    /**
+     * count bytes until we find a '\0'.
+     */
     for (; *pos != '\0'; pos++)
     {
         i++;
@@ -42,17 +63,30 @@ size_t strlen(const char *s)
     return i;
 }
 
+/**
+ * strncpy:
+ * @brief copies up to n bytes from one pointer to another.
+ * 
+ * @param dest - the destination pointer
+ * @param src  - the source pointer
+ * @param n    - the number of bytes to copy.
+ * @return a pointer to dest.
+ */
 char *strncpy(char *dest, const char *src, size_t n)
 {
     size_t i = 0;
 
-    /* copy up to n bytes or until null byte. */
+    /**
+     * copy up to n bytes or until null byte. 
+     */
     for (i = 0; i < n && src[i] != '\0'; i++)
     {
         dest[i] = src[i];
     }
 
-    /* zero out the rest. */
+    /**
+     * zero out the rest. 
+     */
     for (; i < n; i++)
     {
         dest[i] = '\0';
@@ -61,32 +95,42 @@ char *strncpy(char *dest, const char *src, size_t n)
     return dest;
 }
 
-void *memcpy(void *dest, const void *src, size_t n)
-{
-    size_t i = 0;
-
-    /* copy n bytes. */
-    for (i = 0; i < n; i++)
-    {
-        ((char *)dest)[i] = ((char *)src)[i];
-    }
-    return dest;
-}
-
-char *string_tok(char *str, char del)
+/**
+ * strtok:
+ * @brief tokenizes a string on a given delimeter.
+ * 
+ * NOTE: this function varies from the libc strtok in a number of ways:
+ * (1) it takes a char delimeter,
+ * (2) it does not track a pointer globally and
+ * (3) it returns the next occurrence, not the first.
+ * 
+ * @param str - the string to tokenize.
+ * @param del - a character to serve as the delimeter.
+ * @return a pointer to one byte beyond the next delimeter; NULL if none is found.
+ */
+char *strtok(char *str, char del)
 {
     int i = 0;
     for (i = 0; str[i] != del && str[i] != '\0'; i++) {}
 
     if (str[i] == '\0')
     {
-        return 0;
+        return NULL;
     }
 
     str[i++] = '\0';
     return str + i;
 }
 
+/**
+ * memset:
+ * @brief sets a range of addresses to a given value.
+ * 
+ * @param s - a pointer to the start of the range.
+ * @param c - the value to set the memory to.
+ * @param n - the number of bytes to set.
+ * @return a pointer to the start of the range.
+ */
 void *memset(void *s, int c, size_t n)
 {
     char *ptr = s;
@@ -100,6 +144,14 @@ void *memset(void *s, int c, size_t n)
     return (void *)ptr;
 }
 
+/**
+ * printstr:
+ * @brief a wrapper function for calling the write syscall on a string.
+ * 
+ * @param fd - the file descriptor to write to.
+ * @param str - the string to print.
+ * @return void.
+ */
 void printstr(int fd, char *str)
 {
     unsigned long len = strlen(str);
--- strings.h
+++ strings.h
@@ -1,13 +1,34 @@
 #ifndef __STRINGS_H
 #define __STRINGS_H
 
-void zero_string(char *str, unsigned length);
+/**
+ * @brief checks each value in a string for equality.
+ */
 int strncmp(const char *s1, const char *s2, size_t n);
+
+/**
+ * @brief returns the number of bytes in a string (excluding the terminating NULL byte).
+ */
 size_t strlen(const char *s);
+
+/**
+ * @brief copies up to n bytes from one pointer to another.
+ */
 char *strncpy(char *dest, const char *src, size_t n);
-void *memcpy(void *dest, const void *src, size_t n);
+
+/**
+ * @brief tokenizes a string on a given delimeter.
+ */
+char *strtok(char *str, char del);
+
+/**
+ * @brief sets a range of addresses to a given value.
+ */
 void *memset(void *s, int c, size_t n);
-char *string_tok(char *str, char del);
+
+/**
+ * @brief a wrapper function for calling the write syscall on a string.
+ */
 void printstr(int fd, char *str);
 
 #endif
